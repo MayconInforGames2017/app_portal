60155502efa6d8a0de30f2da269dd7754b8feca6
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-09-12 12:56:36 -0300

    Implementacao da area de pesquisa

--- src/app/matriculas/matriculas.page.ts
+++ src/app/matriculas/matriculas.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatriculasPage implements OnInit {
 
+  curso : string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  carregar() {
+
   }
 
 }
